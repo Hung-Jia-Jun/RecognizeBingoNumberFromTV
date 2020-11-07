39d8c6654b511ac0ae6c3196f8d255f25c63f06b
Jason <yf9000555@gmail.com>
2020-11-07 20:32:34 +0800

    更新Config的方式切換不同辨識圖的Patten

--- ReadCamera.py
+++ ReadCamera.py
@@ -11,18 +11,7 @@ def Record():
     print(Start_time)
     # 選擇第1隻攝影機
     cap = cv2.VideoCapture(0)
-    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
-    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
-
-    # # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # # fourcc = cv2.VideoWriter_fourcc('F', 'L', 'V', '1')
-    # # fourcc = cv2.VideoWriter_fourcc('I', '4', '2', '0')
-    # fourcc = 0x00000021
-    # fps = cap.get(cv2.CAP_PROP_FPS)
-    # #每五分鐘拍一段90秒影像
-    # out = cv2.VideoWriter(filename + '.mp4', fourcc, 30,
-    #                       (int(width), int(height)))
-
+    
     i = 0
     while(True):
         # 從攝影機擷取一張影像
@@ -33,8 +22,8 @@ def Record():
 
         cv2.waitKey(1)
         i += 1
-        #每0.5秒截一次圖
-        if i > 15:
+        #每秒截一次圖
+        if i > 30:
             filename = "{hour}_{min}_{sec}".format(
                 hour=time.localtime().tm_hour, min=time.localtime().tm_min, sec=time.localtime().tm_sec)
             cv2.imwrite("C:\\Users\\NO NAME\\Desktop\\out\\"+filename+".jpg", frame)
