eeaf557ce0b5c8e9670987e56a27150a6be6b1d4
Jason <yf9000555@gmail.com>
2020-11-10 11:34:13 +0800

    更新寫入diff log

--- ReadCamera_Recognize.py
+++ ReadCamera_Recognize.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import cv2
 import time
-
+import Recaptcha_Lib
 from datetime import datetime
+from keras.models import load_model
+from PIL import Image
 
-
-def Record():
+#因為要依照數字個別輸出
+#所以要把上個結果存起來
+lastOutputStr = ""
+def Record(imageType):
+    global lastOutputStr
     Start_time = time.time()
     
     # 輸出結果
@@ -12,6 +19,8 @@ def Record():
     # 選擇第1隻攝影機
     cap = cv2.VideoCapture(0)
     
+    file_object = open('recognizeResult.txt', 'a')
+
     i = 0
     while(True):
         # 從攝影機擷取一張影像
@@ -24,14 +33,35 @@ def Record():
         i += 1
         #每秒截一次圖
         if i > 30:
+            img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            # img = Image.open(str(imageType) + '_{index}.jpg'.format(index = str(j)))
+            recognizeResult, secondSplitImg = Recaptcha_Lib.combineResult(img=img,
+                                                        imageType=str(imageType))
+            # 顯示圖片
+            cv2.imshow('process done', secondSplitImg)
+
+            cv2.waitKey(1)
             filename = "{hour}_{min}_{sec}".format(
                 hour=time.localtime().tm_hour, min=time.localtime().tm_min, sec=time.localtime().tm_sec)
             cv2.imwrite("C:\\Users\\NO NAME\\Desktop\\out\\"+filename+".jpg", frame)
             i = 0
 
+            #定義要寫到Log的字串
+            outputStr = ""
+            #每兩個數字為一排
+            step = 2
+             
+            #先做Diff運算，取出最新更新的數字
+            diffRecognizeResult = recognizeResult.replace(lastOutputStr, "")
+            print(diffRecognizeResult)
+            lastOutputStr = recognizeResult
+            #寫Log紀錄檔
+            file_object.write(diffRecognizeResult+"\n")
+
         #現在時間與啟動錄影時間>100秒就離開
         now = time.time()
         if now - Start_time > 100:
+            file_object.close()
             break
     # 釋放攝影機
     cap.release()
@@ -39,6 +69,14 @@ def Record():
     cv2.destroyAllWindows()
 
 
+Ani = {"0": "格狀列表",
+            "1": "打地鼠",
+            "2": "動物農場",
+            "3": "賽車",
+            "4": "套圈圈",
+            "5": "舞龍舞獅",
+            "6": "彩球",
+            "7": "魚"}
 while True:
     # 轉換為 struct_time 格式的本地時間
     result = time.localtime(time.time())
@@ -48,5 +86,12 @@ while True:
 
     #可以被5分鐘整除
     time.sleep(1)
+    imageType = Recaptcha_Lib.GetNextAni()
+    print("現在開獎的動畫為："+Ani[str(imageType)])
+
+    #因為動畫的dic從0開始,但是config從 1 開始，所以要+1
+    imageType = imageType+1
     if result[4] % 5 == 0:
-        Record()
+        #開始錄影
+        Record(imageType)
+    
--- Recaptcha_Lib.py
+++ Recaptcha_Lib.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # import pytesseract
+from bs4 import BeautifulSoup
+import requests
 from PIL import Image
 from matplotlib import pyplot as plt
 import re
+import os
 import cv2
 import numpy as np
 from keras.models import load_model
@@ -16,15 +19,18 @@ import cv2
 import numpy as np
 import configparser
 
+model = load_model('Models/model.h5')
+
 def PredictImg(img):
     # 載入模型
     #縮放圖片大小
-    Max_Width=20
-    Max_Hight=20
+    Max_Width = 20
+    Max_Hight = 20
     x_test = []
     # for img in imgs:
     try:
-        img = cv2.resize(img, (Max_Width, Max_Hight), interpolation=cv2.INTER_CUBIC)
+        img = cv2.resize(img, (Max_Width, Max_Hight),
+                         interpolation=cv2.INTER_CUBIC)
     except:
         return None
     x_test.append(img)
@@ -33,21 +39,24 @@ def PredictImg(img):
     x_test = 255 - x_test
     x_test = x_test.astype('float32')
     x_test /= 255
-    pred = model.predict(x_test,batch_size = 25)
-    outdict = ['0','1','2','3','4','5','6','7','8','9']
+    pred = model.predict(x_test, batch_size=25)
+    outdict = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     pred_result = outdict[np.argmax(pred)]
     return pred_result
 
 #去除掉圖片的雜訊，使用filter來決定可不可以用
-def ImageFilter(width,height):
+
+
+def ImageFilter(width, height):
     # 面積太小就算雜訊
-    if width<=3 or width>20 or width*height<70 \
-        or width*height>400 or height <= 14 or height > 22 or height - width < 5:
-        return False 
+    if width <= 3 or width > 20 or width*height < 70 \
+            or width*height > 400 or height <= 14 or height > 22 or height - width < 5:
+        return False
     else:
         return True
 
-def boundleSort(contours,columnLength,imageType):
+
+def boundleSort(contours, columnLength, imageType):
     # sortPatten = {x:0,y:1}
     # sort = [x,y]
 
@@ -61,8 +70,8 @@ def boundleSort(contours,columnLength,imageType):
     boundle = []
     boundles = []
 
-    for i in range(0,len(contours)):  
-        x, y, width, height = cv2.boundingRect(contours[i])  
+    for i in range(0, len(contours)):
+        x, y, width, height = cv2.boundingRect(contours[i])
         # 面積太小就算雜訊
         if ImageFilter(width, height) == False:
             continue
@@ -70,127 +79,125 @@ def boundleSort(contours,columnLength,imageType):
         #把符合規則的邊界存起來
         boundle.append([x, y, width, height])
 
-
     #list 分群
-    splitRows = [boundle[i:i+columnLength] for i in range(0,len(boundle),columnLength)]
+    splitRows = [boundle[i:i+columnLength]
+                 for i in range(0, len(boundle), columnLength)]
     for bound in splitRows:
         if imageType != "1":
             # 使用Ｘ軸來排序
             boundles.append(sorted(bound, key=lambda bound: bound[0]))
         else:
-            boundles.append([bound[1],bound[0]])
+            boundles.append([bound[1], bound[0]])
 
     return boundles
-def DrawBoundle(boundle,img,columnLength):
-    for i in range(0,len(boundle)):
+
+
+def DrawBoundle(boundle, img, columnLength):
+    for i in range(0, len(boundle)):
         for j in range(len(boundle[i])):
             x, y, width, height = boundle[i][j]
             # print (x, y, width, height)
             cv2.rectangle(img, (x, y),
-                                        (x + width, 
-                                        y + height), (153,153,0), 2) 
+                          (x + width,
+                           y + height), (153, 153, 0), 2)
             # cv2.putText(img, str((i*columnLength)+j), (x,y), cv2.FONT_HERSHEY_SIMPLEX,
             #     0.5, (255, 255, 255), 1, cv2.LINE_AA)
     return img
-def imageProcess(img,THRESH_BINARY_TYPE,threshValue,area,columnLength,imageType):
+
+
+def imageProcess(img, THRESH_BINARY_TYPE, threshValue, area, columnLength, imageType):
     # plt.imshow(img)
     # plt.title('original picture')
     # plt.show()
 
-
-
     #切割掉不重要的區域
     crop_img = img.crop(area)
-    
+
     # plt.imshow(crop_img)
     # plt.title('crop_img picture')
     # plt.show()
 
-
-    img = cv2.cvtColor(np.asarray(crop_img),cv2.COLOR_RGB2BGR)  
+    img = cv2.cvtColor(np.asarray(crop_img), cv2.COLOR_RGB2BGR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-    originalImg = img.copy()                              
-    originalImg = cv2.GaussianBlur(originalImg, (1,1), 1)
+    originalImg = img.copy()
+    originalImg = cv2.GaussianBlur(originalImg, (1, 1), 1)
 
     if THRESH_BINARY_TYPE == "THRESH_BINARY_INV":
         #二值化
-        ret,originalImg = cv2.threshold(originalImg,threshValue,255,        
-                                        cv2.THRESH_BINARY_INV)
+        ret, originalImg = cv2.threshold(originalImg, threshValue, 255,
+                                         cv2.THRESH_BINARY_INV)
     else:
         #二值化
-        ret,originalImg = cv2.threshold(originalImg,threshValue,255,        
-                                        cv2.THRESH_BINARY)
+        ret, originalImg = cv2.threshold(originalImg, threshValue, 255,
+                                         cv2.THRESH_BINARY)
     #x軸（橫向）
     #y軸（直向）
     #保存圖像處理好的圖片
     #膨脹
-    imageProcessDone = cv2.erode(originalImg, (1,2), iterations = 2)
+    imageProcessDone = cv2.erode(originalImg, (1, 2), iterations=2)
     # plt.imshow(imageProcessDone)
     # plt.title('imageProcessDone')
     # plt.show()
 
-
     secondSplitImg = imageProcessDone.copy()
     #把完全看不到文字只剩下填滿黑色的色塊版本去做輪廓辨識
-    secondSplitImg ,Splitcontours,hierarchy = cv2.findContours(secondSplitImg,          
-                                                    cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    Splitcontours, hierarchy = cv2.findContours(secondSplitImg,
+                                                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     #送去做Boundle排序
-    boundle = boundleSort(contours = Splitcontours,
-                          columnLength = columnLength,
-                          imageType = imageType)
+    boundle = boundleSort(contours=Splitcontours,
+                          columnLength=columnLength,
+                          imageType=imageType)
 
-    secondSplitImg = DrawBoundle(boundle = boundle,
-                                 img = secondSplitImg,
-                                 columnLength = 20)
-    plt.imshow(secondSplitImg)
-    plt.title("secondSplitImg")
-    plt.show()
-    return boundle,imageProcessDone
-
+    secondSplitImg = DrawBoundle(boundle=boundle,
+                                 img=secondSplitImg,
+                                 columnLength=20)
+    # plt.imshow(secondSplitImg)
+    # plt.title("secondSplitImg")
+    # plt.show()
+    return boundle, imageProcessDone, secondSplitImg
 
 #顯示辨識結果
-def showRecognizeResult(img,THRESH_BINARY_TYPE,threshValue,area,columnLength,imageType):
-    boundle,imageProcessDone = imageProcess(img = img,
-                                            THRESH_BINARY_TYPE = THRESH_BINARY_TYPE,
-                                            threshValue = threshValue,
-                                            area = area,
-                                            columnLength = columnLength,
-                                            imageType = imageType)
+
+
+def showRecognizeResult(img, THRESH_BINARY_TYPE, threshValue, area, columnLength, imageType):
+    boundle, imageProcessDone, secondSplitImg = imageProcess(img=img,
+                                             THRESH_BINARY_TYPE=THRESH_BINARY_TYPE,
+                                             threshValue=threshValue,
+                                             area=area,
+                                             columnLength=columnLength,
+                                             imageType=imageType)
 
     #儲存所有號碼圖的辨識結果
     output = []
-    DrawContours = imageProcessDone.copy()
     _originalImg = imageProcessDone.copy()
-    for i in range(0,len(boundle)):
+    for i in range(0, len(boundle)):
         for j in range(len(boundle[i])):
             x, y, width, height = boundle[i][j]
             # 面積太小就算雜訊
             if ImageFilter(width, height) == False:
                 continue
-            newImage=_originalImg[y:y+height,
-                            x:x+width]
+            newImage = _originalImg[y:y+height,
+                                    x:x+width]
             predValue = PredictImg(newImage)
             output.append(predValue)
             # plt.imshow(newImage)
             # plt.title(predValue+":"+str(x)+","+str( y)+","+str( width)+","+str( height))
             # plt.show()
-            input_dir=("cut_image/")
+            input_dir = ("cut_image/")
             if not os.path.isdir(input_dir):
                 os.makedirs(input_dir)
             cv2.imwrite("./cut_image/{i}_{predValue}.jpg".format(i=str(i),
-                        predValue = predValue),newImage) 
-    return output
-
-
+                                                                 predValue=predValue), newImage)
+    return output, secondSplitImg
 
 #傳入圖片與圖片類型
-def combineResult(img,imageType):
-    print ("Type:" + imageType)
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+def combineResult(img, imageType):
+    print("Type:" + str(imageType))
     #圖片要辨識的類型，共八種
 
     #x軸（橫向）
@@ -206,14 +213,13 @@ def combineResult(img,imageType):
     #橫行直列的數量
     columnLength = int(_config["columnLength"])
 
-
     # img = Image.open(imageType + '_3.jpg')
-    output = showRecognizeResult(img = img,THRESH_BINARY_TYPE = TYPE,threshValue = threshValue,area = area,columnLength = columnLength,imageType = imageType)
+    output, secondSplitImg = showRecognizeResult(img=img, THRESH_BINARY_TYPE=TYPE, threshValue=threshValue,
+                                 area=area, columnLength=columnLength, imageType=imageType)
     #每兩個數字為一排
     step = 2
-    output = [output[i:i+step] for i in range(0,len(output),step)]
-    
-    
+    output = [output[i:i+step] for i in range(0, len(output), step)]
+
     #賽車與套圈圈的數字要做特別處理
     if imageType == "4" or imageType == "5":
         #因為有兩排，現在想先知道切片要切第一排與第二排要留多少
@@ -221,11 +227,11 @@ def combineResult(img,imageType):
         # output.reverse()
         outFirstLine = []
         outSecondLine = []
-        
+
         for i in range(len(output)):
-            try:    
+            try:
                 #輸出1,3,5,7,9 行，因為數字是直列下來的
-                if i%2 == 1:
+                if i % 2 == 1:
                     outSecondLine.append(output[i])
                     # print (ele[0]+ele[1]+",",end='')
                 else:
@@ -237,23 +243,37 @@ def combineResult(img,imageType):
             except:
                 continue
         output = outSecondLine+outFirstLine
-    
+
+
+    outputStr = ""
     for ele in output:
         try:
-            print (ele[0]+ele[1] + "," , end='')
+            outputStr += ele[0]+ele[1] + ","
+            # print(outputStr)
         except:
             continue
-    return output
+    return outputStr, secondSplitImg
 
+#取得下一次開獎的動畫
+def GetNextAni():
+    #現在動畫  109063595%8 + 3 = 5(套圈圈)
+    r = requests.get("https://www.taiwanlottery.com.tw/Lotto/BINGOBINGO/drawing.aspx")
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    div = soup.find(id="lblBBDrawTerm")
+    bingoNumber = int(div.text)
+    nextBingoAniType = (bingoNumber + 2) % 8
+    return nextBingoAniType
 if __name__ == "__main__":
     model = load_model('Models/model.h5')
     #輸入程式啟動時，下一個是什麼圖形
     firstPatten = int(input("請輸入下一個會顯示的動畫類型"))
-    imageType = firstPatten
-    img = Image.open(imageType + '_3.jpg')
-    combineResult(img = img,imageType = imageType)
-    firstPatten += 1 
+    for i in range(8):
+        imageType = firstPatten
+        img = Image.open(str(imageType) + '_2.jpg')
+        combineResult(img=img, imageType=str(imageType))
+        firstPatten += 1
 
-    #回到初始的類別
-    if firstPatten > 8:
-        firstPatten = 1 
+        #回到初始的類別
+        if firstPatten > 8:
+            firstPatten = 1
